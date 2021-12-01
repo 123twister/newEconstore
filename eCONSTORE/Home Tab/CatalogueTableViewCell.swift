203a84f32508467758c4e0f401978ae790b5285d
Jay Kaushal <123jayvedant@gmail.com>
2021-12-02 01:23:44 +0530

    Catalogue Page completed

---
+++ eCONSTORE/Home Tab/CatalogueTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CatalogueTableViewCell.swift
+//  eCONSTORE
+//
+//  Created by Jay Kaushal on 02/12/21.
+//
+
+import UIKit
+
+class CatalogueTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var typeImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ eCONSTORE/Home Tab/CatalogueViewController.swift
@@ -0,0 +1,84 @@
+//
+//  CatalogueViewController.swift
+//  eCONSTORE
+//
+//  Created by Jay Kaushal on 02/12/21.
+//
+
+import UIKit
+import Firebase
+
+struct Catalogue {
+    let title: String?
+    let image: String?
+}
+
+class CatalogueViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var catalogue: [Catalogue] = []
+    var ref: CollectionReference!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        ref = Firestore.firestore().collection("categories")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+        GetData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catalogue.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CatalogueTableViewCell
+        cell.titleLbl.text = catalogue[indexPath.row].title
+        
+        let imgUrl = URL(string: "\(catalogue[indexPath.row].image ?? "")")
+        if let data = try? Data(contentsOf: imgUrl! ) {
+            DispatchQueue.main.async {
+                cell.typeImg.image = UIImage(data: data)
+            }
+        }
+        
+        return cell
+    }
+    
+    func GetData()
+    {
+        ref.getDocuments { (snapshot, error) in
+            if let error = error {
+                // SHOW ERROR
+                
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                let okBtn = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(okBtn)
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                // FETCHING DATA
+                
+                guard let snapshot = snapshot else { return }
+                for documents in snapshot.documents {
+                    let data = documents.data()
+                    let title = data["title"] as? String ?? ""
+                    let image = data["image"] as? String ?? ""
+                    
+                    let newCatalogue = Catalogue(title: title, image: image)
+                    self.catalogue.append(newCatalogue)
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+}
+
